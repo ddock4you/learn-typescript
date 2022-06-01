5a8fab8944e433a8407cd409ad6c31bca6a1f350
ddock4you <ddock4you@gmail.com>
2022-06-01 14:01:38 +0900

    first -finish

--- 01_first/js-only.js
+++ 01_first/js-only.js
@@ -7,3 +7,40 @@ function add(num1, num2) {
 button.addEventListener("click", function () {
     console.log(add(+input1.value, +input2.value));
 });
+var num1 = 1;
+var num2 = 2;
+var stringResult = '정답은.. ';
+var isShow = false;
+var add2 = function (num1, num2, result, isShow) {
+    var calculate = num1 + num2;
+    if (isShow)
+        return alert(result + calculate);
+    else
+        console.log(calculate);
+};
+add2(num1, num2, stringResult, isShow);
+var role;
+(function (role) {
+    role["num1"] = "hi";
+    role["num2"] = "by";
+    role["num3"] = "aa";
+})(role || (role = {}));
+;
+var obj1 = {
+    name: 'ysh',
+    age: 33,
+    role: ['a', 'b'],
+    deepArray: [2, 'hi'],
+    deepObj: {
+        name: 'y'
+    },
+    grade: role.num1
+};
+console.log(obj1);
+var arr1 = ['A', 'B'];
+var numberOrString = 'A';
+var customFunction = add2;
+var add3 = function (num1, num2, cb) {
+    console.log(cb(num1 + num2));
+};
+add3(num1, num2, function (sumresult) { return "\uD569\uACC4: ".concat(sumresult); });
--- 01_first/js-only.ts
+++ 01_first/js-only.ts
@@ -9,3 +9,62 @@ function add(num1:number, num2:number) {
 button.addEventListener("click", function() {
   console.log(add(+input1.value, +input2.value));
 });
+
+
+
+const num1 = 1;
+const num2 = 2;
+const stringResult = '정답은.. ';
+const isShow = false;
+
+const add2 = (num1: number, num2:number, result:string, isShow: boolean) => {
+    const calculate = num1 + num2;
+    if (isShow) return alert(result + calculate);
+    else console.log(calculate);
+}
+
+add2(num1, num2, stringResult, isShow);
+
+enum role { num1 = 'hi', num2 = 'by', num3 = 'aa'};
+
+const obj1: {
+    name: string;
+    age: number;
+    role: string[];
+    deepArray: [number, string],
+    deepObj: {
+        name: string
+    },
+    grade: string
+} = {
+    name: 'ysh',
+    age: 33,
+    role: ['a', 'b'],
+    deepArray: [2, 'hi'],
+    deepObj: {
+        name: 'y'
+    },
+    grade: role.num1
+}
+
+console.log(obj1);
+
+const arr1: string[] = ['A', 'B'];
+
+type combine = string | number;
+
+const numberOrString:combine = 'A';
+
+const customFunction:Function = add2 ;
+
+const add3 = (num1: number, num2: number, cb:(sum:number) => string) => {
+    cb(num1 + num2);
+}
+
+add3(num1, num2, (sumresult) => `합계: ${sumresult}`);
+
+let hi:unknown;
+let bye:any;
+// unknown 타입은 다른 타입이 지정된 변수에 할당할 수 없음.
+// const aaa:string = hi;
+const bbb:number = bye;
